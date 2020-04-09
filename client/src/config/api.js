d52d7aa48f51b4e47b577cee18c19f77ba21af62
harkon666 <bryan.wicaksanaa@gmail.com>
2020-04-09 11:09:14 +0700

    change base url to heroku

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 export const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://land-tick.herokuapp.com/api",
 });
 
-export const setAuthToken = token => {
+export const setAuthToken = (token) => {
   API.defaults.headers.common["Authorization"] = `${token}`;
 };
